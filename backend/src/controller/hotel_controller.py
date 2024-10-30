537e944720179b69f354d4cbcd38c8f0f81d3044
minhyeok0328 <alsgur7408@gmail.com>
2024-10-30 17:38:17 +0900

    feat(api): dowhat에서 제공받은 csv파일 파싱해서 table에 insert하는 API 추가 (#13)

--- backend/src/controller/hotel_controller.py
+++ backend/src/controller/hotel_controller.py
@@ -1,10 +1,12 @@
 from fastapi import File, UploadFile
-from src.decorator import controller, get
+from src.decorator import controller, get, post
+from src.service import HotelService
 
 
-# 참고용 controller 
 @controller('/hotel')
 class HotelController:
+    def __init__(self) -> None:
+        self.hotel_service = HotelService()
 
     @get('/order')
     async def order(self, start_date: str, end_date: str):
@@ -13,7 +15,10 @@ class HotelController:
     @get('/list')
     async def list(self):
         return {}
-        
-    @get('/upload_csv')
-    async def upload_csv(self, file: UploadFile):
-        return {}
+
+    @post('/upload_csv')
+    async def upload_csv(self, hotel_seq: int, csv_file: UploadFile = File()):
+        csv_content = await csv_file.read()
+        response = await self.hotel_service.insert_hotel_data_from_csv(hotel_seq, csv_content)
+
+        return response
---
+++ backend/src/service/hotel_service.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import io
+import json
+from sqlalchemy import Table, create_engine, MetaData, insert
+from sqlalchemy.exc import SQLAlchemyError
+
+
+DB: dict = {
+    'address': '192.168.0.9',
+    'user': 'next-loop',
+    'password': '1234',
+    'database': 'next-loop'
+}
+
+class HotelService:
+    def __init__(self) -> None:
+        try:
+            self.db = create_engine(f"mariadb+pymysql://{DB['user']}:{DB['password']}@{DB['address']}/{DB['database']}")
+            self.metadata = MetaData()
+            self.hotel_order_table = Table('hotel_order', self.metadata, autoload_with=self.db)
+            print(f'hotel table column: {self.hotel_order_table.columns.keys()}')
+        except Exception as error:
+            print('Database Connection Error:')
+            print(error)
+
+    async def insert_hotel_data_from_csv(self, hotel_seq: int, csv_content: bytes):
+        csv_reader = io.StringIO(csv_content.decode('utf-8'))
+        csv_reader.seek(0)
+
+        df = pd.read_csv(csv_reader)
+
+        with self.db.connect() as connection:
+            trans = connection.begin()
+            try:
+                for _, row in df.iterrows():
+                    row_dict = row.to_dict()
+                    row_dict = {k: (None if v == '\\N' else v) for k, v in row_dict.items()}
+
+                    if 'contents' in row_dict and isinstance(row_dict['contents'], dict):
+                        row_dict['contents'] = json.dumps(row_dict['contents'])
+
+                    insert_query = insert(self.hotel_order_table).values(**row_dict)
+                    connection.execute(insert_query)
+
+                trans.commit()
+            except SQLAlchemyError as e:
+                trans.rollback()
+                print(f"insert_hotel_data_from_csv | Database Query Error: {e}")
+                return False
+
+        return True
