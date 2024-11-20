7c9cbd68287754adc17681f9dc5e466fe605bb08
minhyeok0328 <alsgur7408@gmail.com>
2024-11-21 01:15:03 +0900

    feat(feast): 전처리된 데이터에 맞게 feast도 수정 (#87)
    일단 업데이트는 해 두는데 feast를 쓸 지는 모르겠다

--- feature_store/feature_repo/hotel_order.py
+++ feature_store/feature_repo/hotel_order.py
@@ -1,21 +1,19 @@
 from datetime import timedelta
-from feast import Entity, Feature, FeatureView, FileSource
-from feast.types import Int64, String, Float32
-from feast.value_type import ValueType
+from feast import Entity, FeatureView, FileSource
+from feast.types import Int64, String, Float32, Timestamp
 from feast.field import Field
 
-# 데이터 소스 정의
+
 csv_file_source = FileSource(
-    path="data/data.parquet", 
+    path = "gs://dowhat-de1-datawarehouse/api/v1/hotel_order/**/*.parquet",
     event_timestamp_column="reg_date",
     created_timestamp_column="check_date",
 )
 
-# 엔티티 정의
 order_entity = Entity(
     name="order_seq",
-    value_type=ValueType.INT64,
-    description="Unique identifier for each order"
+    join_key="order_seq",
+    description="Unique identifier for each order",
 )
 
 # Feature View 정의
@@ -24,7 +22,7 @@ order_feature_view = FeatureView(
     entities=[order_entity],
     ttl=timedelta(days=1),
     schema=[
-        Field(name="room_seq", dtype=String),
+        Field(name="room_seq", dtype=Int64),
         Field(name="customer_seq", dtype=Int64),
         Field(name="order_price", dtype=Float32),
         Field(name="status", dtype=String),
@@ -34,14 +32,26 @@ order_feature_view = FeatureView(
         Field(name="visit_adult_cnt", dtype=Int64),
         Field(name="visit_child_cnt", dtype=Int64),
         Field(name="visit_baby_cnt", dtype=Int64),
+        Field(name="check_in", dtype=Timestamp),
+        Field(name="check_out", dtype=Timestamp),
+        Field(name="check_out_expected", dtype=Timestamp),
+        Field(name="item_seq", dtype=Int64),
+        Field(name="item_name", dtype=String),
+        Field(name="item_count", dtype=Int64),
+        Field(name="department_seq", dtype=Int64),
+        Field(name="price", dtype=Float32),
+        Field(name="waiting_period", dtype=Int64),
+        Field(name="delay_period", dtype=Int64),
+        Field(name="free_count", dtype=Float32),
+        Field(name="coupon_seq", dtype=Int64),
+        Field(name="result_price", dtype=Float32),
+        Field(name="complete_period", dtype=Float32),
     ],
     source=csv_file_source,
 )
 
-# Feature View 등록
 if __name__ == "__main__":
     from feast import FeatureStore
 
     store = FeatureStore(repo_path=".")
     store.apply([order_feature_view, order_entity])
-
